ba589e80dc9e26437dc37a22cb9e1727958b3375
Cloud Mojos <joshuel.simbulan@gmail.com>
2022-03-26 00:38:54 +0800

    use float/inline-block

--- script.js
+++ script.js
@@ -3,12 +3,21 @@ const etch_grid = document.getElementById("grid-container");
 const btn_size_option = document.querySelectorAll(".size-option");
 
 function createGrid (size) {
-    for (let i = 0; i < size; i++)
+    // Compute the correct cell size
+    // by taking the dimension of etch_grid
+    // and dividing it by size
+    const etch_grid_width = etch_grid.clientWidth; // scoped here so it gives
+    const etch_grid_height = etch_grid.clientHeight; // the correct dimension when clicked
+    const cell_size = (etch_grid_height / size) - 0.1;
+
+    for (let i = 0, n = size * size; i < n; i++)
     {
         const etch_cell = document.createElement('div');
-        etch_cell.setAttribute('style', 'display: inline-block; height: 25px; width: 25px; background-color: black; float: left;');
+        etch_cell.setAttribute('style', `height: ${cell_size}px; width: ${cell_size}px;`);
+        etch_cell.classList.add('etch-cell');
         etch_grid.appendChild(etch_cell);
     }
+    alert(etch_grid_height + ", " + cell_size);
 }
 
 btn_size_option.forEach((btn) => {
