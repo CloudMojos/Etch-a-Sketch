4c84d0a98fee79baadb9a51c26dd9361366b32a5
Cloud Mojos <joshuel.simbulan@gmail.com>
2022-03-26 13:46:23 +0800

    use css grid

--- script.js
+++ script.js
@@ -3,21 +3,23 @@ const etch_grid = document.getElementById("grid-container");
 const btn_size_option = document.querySelectorAll(".size-option");
 
 function createGrid (size) {
-    // Compute the correct cell size
-    // by taking the dimension of etch_grid
-    // and dividing it by size
-    const etch_grid_width = etch_grid.clientWidth; // scoped here so it gives
-    const etch_grid_height = etch_grid.clientHeight; // the correct dimension when clicked
-    const cell_size = (etch_grid_height / size) - 0.1;
-
+    clearGrid();
+    etch_grid.setAttribute('style', `grid-template-columns: repeat(${size}, 1fr); 
+                                    grid-template-rows: repeat(${size}, 1fr)`);
     for (let i = 0, n = size * size; i < n; i++)
     {
         const etch_cell = document.createElement('div');
-        etch_cell.setAttribute('style', `height: ${cell_size}px; width: ${cell_size}px;`);
         etch_cell.classList.add('etch-cell');
         etch_grid.appendChild(etch_cell);
     }
-    alert(etch_grid_height + ", " + cell_size);
+    //alert(etch_grid_height + ", " + cell_size);
+}
+
+function clearGrid() {
+    const all_etch_cells = document.querySelectorAll('.etch-cell');
+    all_etch_cells.forEach((cell) => {
+        etch_grid.removeChild(cell);
+    })
 }
 
 btn_size_option.forEach((btn) => {
