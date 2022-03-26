7746bc57794221310498e2b6d25c5893d8b456b6
Cloud Mojos <joshuel.simbulan@gmail.com>
2022-03-26 14:41:57 +0800

    add grid lines

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const etch_grid = document.getElementById("grid-container");
 const btn_size_option = document.querySelectorAll(".size-option");
 
+// Creates grid based on size
 function createGrid (size) {
     clearGrid();
     etch_grid.setAttribute('style', `grid-template-columns: repeat(${size}, 1fr); 
@@ -9,7 +10,7 @@ function createGrid (size) {
     for (let i = 0, n = size * size; i < n; i++)
     {
         const etch_cell = document.createElement('div');
-        etch_cell.classList.add('etch-cell');
+        etch_cell.classList.add('etch-cell', 'grid-lines');
         etch_grid.appendChild(etch_cell);
     }
     //alert(etch_grid_height + ", " + cell_size);
