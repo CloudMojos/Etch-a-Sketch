144cdbe281575ef3e4269a87bdda0953466e1dd4
Cloud Mojos <joshuel.simbulan@gmail.com>
2022-03-27 23:39:05 +0800

    make paint prototype

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Handles
 const etch_grid = document.getElementById("grid-container");
 const btn_size_option = document.querySelectorAll(".size-option");
+const paint = document.getElementById("paintbrush");
+let all_etch_cells = document.querySelectorAll('.etch-cell');
+createGrid(8);
 
 // Creates grid based on size
 function createGrid (size) {
@@ -13,18 +16,33 @@ function createGrid (size) {
         etch_cell.classList.add('etch-cell', 'grid-lines');
         etch_grid.appendChild(etch_cell);
     }
+    all_etch_cells = document.querySelectorAll('.etch-cell');
     //alert(etch_grid_height + ", " + cell_size);
 }
 
+
+
+// Clears the grid. Taking each cell, removing it
 function clearGrid() {
-    const all_etch_cells = document.querySelectorAll('.etch-cell');
+    all_etch_cells = document.querySelectorAll('.etch-cell');
     all_etch_cells.forEach((cell) => {
         etch_grid.removeChild(cell);
     })
 }
 
+// Activate painting
+console.log(all_etch_cells);
+all_etch_cells.forEach((cell) => {
+    cell.addEventListener('mousedown', () => {
+        console.log("I ran");
+        cell.setAttribute('style', `background-color: black;`);
+    })
+})
+
+//
 btn_size_option.forEach((btn) => {
     btn.addEventListener("click", () => {
+        all_etch_cells = document.querySelectorAll('.etch-cell');
         createGrid(btn.id);
     })
 })
